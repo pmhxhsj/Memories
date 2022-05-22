caeed1eb90adc1987cf4e097179401ac12100ad0
pmhxhsj <minho7952@naver.com>
2022-05-23 00:52:40 +0900

    feat: footer setting

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
-const Footer = (props) => <h1>Footer</h1>;
+const Footer = (props) => {
+  const emailClick = () => {
+    alert('pmhxxhsj@gmail.com');
+  };
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span onClick={emailClick}>Email</span> &nbsp;|&nbsp;
+        <a href='https://parkparkpark.tistory.com/' target='_blank' rel='noopener noreferrer'>
+          Blog
+        </a>
+        &nbsp;|&nbsp;
+        <a href='https://github.com/pmhxhsj' target='_blank'>
+          Github
+        </a>
+        <br />
+        <span>Copyright 2022. Keny. All Rights Reserved.</span>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
